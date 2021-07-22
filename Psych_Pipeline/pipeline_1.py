091627c06df27f33dd98f35ac889a702baa2badb
Shubha Guha <shubha.g@gmail.com>
2021-07-22 14:38:22 +0200

    Changed label computation, removed mean absolute error, and small cleanup

--- Psych_Pipeline/pipeline_1.py
+++ Psych_Pipeline/pipeline_1.py
@@ -6,55 +6,36 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_absolute_error
 
 # read the data
-raw_data = pd.read_csv(r"C:\Users\nchau\Documents\Internships\ML_Inspect\International students Time management data.csv")
+raw_data = pd.read_csv("International students Time management data.csv")
 pd.set_option("display.max.columns", None)
-#print(raw_data['Academic'])
-#print(raw_data['Attendance'])
-#print(raw_data['Course'])
-
-# prepare for one-hot-encoding
-# le = LabelEncoder()
-# raw_data['Course'] = le.fit_transform(raw_data['Course'])
-# raw_data['Academic'] = le.fit_transform(raw_data['Academic'])
-# raw_data['Attendance'] = le.fit_transform(raw_data['Attendance'])
-# print(raw_data['Academic'])
-# print(raw_data['Course'])
-
-# print(raw_data.columns)
-
-# transform multiple columns
-features_col = ['Course','Academic','Attendance']
-features_col_cat = ['Course']
-features_col_ord = ['Academic', 'Attendance']
 
 enc = OneHotEncoder(sparse=False, handle_unknown='ignore')
 ord = OrdinalEncoder()
 
-prep_pipeline = Pipeline([
+impute_and_one_hot = Pipeline([
     ('impute', SimpleImputer(strategy= 'most_frequent')),
     ('encode', OneHotEncoder(sparse=False, handle_unknown='ignore'))
 ])
 
-prep_pipeline2 = Pipeline([
+impute_and_ordinal = Pipeline([
     ('impute', SimpleImputer(strategy= 'most_frequent')),
     ('ordinal', OrdinalEncoder())
 ])
 
 categorical_prepr = ColumnTransformer([
-    ("prep_pipeline", prep_pipeline, ['Course']),
-    ("prep_pipeline2", prep_pipeline2, ['Academic', 'Attendance'])
-    ])
+    ("impute_and_one_hot", impute_and_one_hot, ['Course']),
+    ("impute_and_ordinal", impute_and_ordinal, ['Academic', 'Attendance'])
+])
 
 # transform y variable
-raw_data['Bin_Code'] = np.where(raw_data['7'].str.contains("Dis", "Neither"), 1, 0)
+raw_data['label'] = (raw_data['7'] == 'Agree') | (raw_data['7'] == 'Strong Agree')
 
 impu_1 = SimpleImputer(strategy='most_frequent').fit_transform(raw_data[['Academic']])
-print(impu_1)
+# print(impu_1)
 fit_1 = enc.fit_transform(impu_1)
-print(fit_1)
+# print(fit_1)
 #
 # impu_2 = SimpleImputer(strategy='most_frequent').fit_transform(raw_data[['Attendance']])
 # print(impu_2)
@@ -72,23 +53,15 @@ pipe = Pipeline([
     ('classifier', LogisticRegression())
 ])
 
-
-X = raw_data[features_col]
-y = raw_data['Bin_Code']
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, random_state=1)
+X_train, X_test, Y_train, Y_test = train_test_split(raw_data, raw_data['label'], random_state=1)
 
 # fit the pipeline to the training data
 pipe.fit(X_train, Y_train)
-print(raw_data['Academic'])
-#print(raw_data['Attendance'])
-#print(raw_data['Course'])
 
 # predict target values on training data
-pipe.predict(X_train)
+pred_train = pipe.predict(X_train)
 
 # validate with X
-validate_predication = pipe.predict(X_test)
-difference = mean_absolute_error(Y_test, validate_predication)
+pred_test = pipe.predict(X_test)
 score = pipe.score(X_test, Y_test)
-print(difference)
-print(score)
+print("Test score:", score)
